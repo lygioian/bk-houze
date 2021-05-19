7a63404f282fd9b19c60b9894cb294a03de30c49
Thanh Nguyen <thanhnguyen2612@hcmut.edu.vn>
2021-05-19 19:12:01 +0700

    Beautify mqtt.service.ts code

--- src/services/mqtt.service.ts
+++ src/services/mqtt.service.ts
@@ -7,7 +7,7 @@ import { DeviceService } from './device.service';
 
 var defaultTopics = [DeviceTopic.LED, DeviceTopic.MAGNETIC];
 
-function getTopicName(topic: string): string {
+function getDeviceName(topic: string): string {
     var name: string = topic.split("/")[2];
     switch (name) {
         case DeviceTopic.LED:
@@ -22,7 +22,7 @@ function getTopicName(topic: string): string {
 }
 
 async function createDeviceHistory(topic: string, message: any): any {
-    var name = getTopicName(topic);
+    var name = getDeviceName(topic);
     if (name === null) return;
     var device = {
         id: 0,
@@ -72,6 +72,7 @@ export class MQTTService {
             console.log(error);
         }
     }
+
     onMessage(topic: any, message: any) {
         console.log(
             "Received '" + message + "' on '" + topic + "'",
@@ -79,31 +80,46 @@ export class MQTTService {
         // Create Device document in MongoDB
         createDeviceHistory(topic, message);
     }
+
     subscribe(topic: DeviceTopic) {
         // Subscribe a topic listed in the list of topics
-        if (this.topics.includes(topic)) {
+        if (this._validateTopic(topic)) {
             console.log("[MQTT] Topic already subscribed:", topic);
             return;
         }
-        var path: string = `${this.options.username}/feeds/${topic}`;
+        var path: string = this._getTopicPath(topic);
         this.topics.push(topic);
         this.client.subscribe(path);
         console.log("[MQTT] Subscribed topic:", path);
     }
+
     unsubscribe(topic: DeviceTopic) {
         // Unsubscribe a topic listed in the list of topics
-        if (!this.topics.includes(topic)) {
+        if (!this._validateTopic(topic)) {
             console.log("[MQTT] Not yet subscribe:", topic);
             return;
         }
-        var path: string = `${this.options.username}/feeds/${topic}`;
+        var path: string = this._getTopicPath(topic);
         this.topics = this.topics.filter(obj => obj !== topic);
         this.client.unsubscribe(path);
         console.log("[MQTT] Unsubscribed topic:", path);
     }
+
     publish(topic: DeviceTopic, message: any) {
-        var path: string = `${this.options.username}/feeds/${topic}`;
+        if (!this._validateTopic(topic)) {
+            console.log("[MQTT] Subscribe '", topic, "' before publishing!");
+            return;
+        }
+        var path: string = this._getTopicPath(topic);
         this.client.publish(path, JSON.stringify(message));
         console.log("Message is published to MQTT");
+    }
+
+    private _getTopicPath(topic: DeviceTopic): string {
+        var path: string = `${this.options.username}/feeds/${topic}`;
+        return path;
+    }
+    private _validateTopic(topic: DeviceTopic): boolean {
+        return this.topics.includes(topic);
     }
 }
