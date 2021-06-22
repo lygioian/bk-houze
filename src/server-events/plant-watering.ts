17f4b3e9580cb264cc3f646b48b5f6fe6140344e
longxayda <longxayda11@gmail.com>
2021-06-22 21:48:49 +0700

    Update plant-watering.ts

--- src/server-events/plant-watering.ts
+++ src/server-events/plant-watering.ts
@@ -1,7 +1,27 @@
-class PlantWatering {
-    constructor() {}
+import { DeviceTopic, getDeviceName } from "../config";
+import { DeviceService, MQTTService } from "../services";
 
-    update(data: any) {}
+class PlantWatering {
+    constructor(
+        private deviceService: DeviceService,
+        private mqttService: MQTTService,
+    ) {
+        console.log('[Plant Watering Management] Construct');
+    }
+
+    async update(data: any) {
+        const device = await this.deviceService.findOne({
+            _id: data.deviceId,
+            name: data.name});
+        if (data.data < 100) {
+            const relayName = getDeviceName(DeviceTopic.RELAY);
+            this.mqttService.publish(11,relayName,'1');
+        }
+        else {
+            const relayName = getDeviceName(DeviceTopic.RELAY);
+            this.mqttService.publish(11,relayName,'0');
+        }
+    }
 }
 
 export default PlantWatering;
